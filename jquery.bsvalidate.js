3771951dd8fc36797573fba1f7fbc84a8536020c
Matt Mink <matthewjmink@gmail.com>
2016-08-30 08:05:30 -0500

    Updating version number (1.0.1)

--- jquery.bsvalidate.js
+++ jquery.bsvalidate.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery bsValidate Plugin v1.0.0
+ * jQuery bsValidate Plugin v1.0.1
  * Form Validation for Twitter Bootstrap Forms (https://github.com/matthewjmink/bsValidate)
  * Copyright 2016 Matt Mink
  * Licensed under MIT (https://github.com/matthewjmink/bsValidate/blob/master/LICENSE)
